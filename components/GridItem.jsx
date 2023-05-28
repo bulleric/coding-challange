d8de93bdc813870c3c1a0a9771f49e14ea727b38
Eric Sidibe <eric@sidibe.de>
2023-05-28 16:36:11 +0800

    activate aspect ratio plugin to dynamicly change the aspect ratio to squared images

--- components/GridItem.jsx
+++ components/GridItem.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const GridItem = () => {
   return (
-    <div className="max-w-sm mx-auto rounded-lg overflow-hidden shadow-md">
-      <div className="relative h-48">
+    <div className="max-w-sm mx-auto bg-white rounded-lg overflow-hidden shadow-md">
+      <div className="relative h-0 pb-full"> {/* This ensures a square aspect ratio */}
         <Image
           src="/assets/images/dummy-image.jpg"
           alt="Dummy Image"
@@ -12,10 +12,10 @@ const GridItem = () => {
         />
       </div>
       <div className="p-4">
-        <h3 className="text-xl font-semibold mb-2">Beautiful Home</h3>
-        <p className="text-gray-600 text-sm mb-4">Entire apartment · 2 guests · 1 bedroom · 1 bed · 1 bath</p>
+        <h3 className="text-lg font-semibold text-gray-800 mb-2">Beautiful Home</h3>
+        <p className="text-gray-600 text-xs mb-4">Entire apartment · 2 guests · 1 bedroom · 1 bed · 1 bath</p>
         <div className="flex items-center">
-          <div className="w-10 h-10 rounded-full overflow-hidden mr-2">
+          <div className="w-8 h-8 rounded-full overflow-hidden mr-2">
             <Image
               src="/assets/user-avatar.jpg"
               alt="Host Avatar"
@@ -23,7 +23,7 @@ const GridItem = () => {
               objectFit="cover"
             />
           </div>
-          <p className="text-gray-700 font-medium">Hosted by John Doe</p>
+          <p className="text-gray-700 text-sm font-medium">Hosted by John Doe</p>
         </div>
       </div>
     </div>
