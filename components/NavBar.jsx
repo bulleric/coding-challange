69ec0bacaa1e16c13d694510c00c15428c6a0e42
Eric Sidibe <eric@sidibe.de>
2023-05-28 16:04:17 +0800

    Fix the navbar component issue with spacing in the Top

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -12,22 +12,22 @@ import UserAvatarButton from "./UserAvatarButton";
 
 const NavBar = () => {
   return (
-        <nav className="fixed w-full z-10 px-6 sm:px-12 py-3 bg-white shadow-md">
-        <div className="max-w-screen-xl mx-auto flex items-center justify-between">
-            <Link href="/">
-            <div>
-                <div className="hidden md:block">
-                <Image src="/assets/logo-desktop.png" alt="Logo" width={200} height={44} />
-                </div>
-                <div className="md:hidden">
-                <Image src="/assets/logo-mobile.png" alt="Logo" width={40} height={40} />
-                </div>
+    <nav className="fixed w-full z-10 top-0 left-0 right-0 z-10 px-6 sm:px-12 py-3 bg-white shadow-md">
+      <div className="max-w-screen-xl mx-auto flex items-center justify-between">
+        <Link href="/">
+          <div>
+            <div className="hidden md:block">
+              <Image src="/assets/logo-desktop.png" alt="Logo" width={200} height={44} />
             </div>
-            </Link>
-            <UserAvatarButton />
-        </div>
-        </nav>
-    );
+            <div className="md:hidden">
+              <Image src="/assets/logo-mobile.png" alt="Logo" width={40} height={40} />
+            </div>
+          </div>
+        </Link>
+        <UserAvatarButton />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
