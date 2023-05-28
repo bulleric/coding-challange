3451ede7db8721503285633ea42f755237cc7a35
Eric Sidibe <eric@sidibe.de>
2023-05-28 13:06:57 +0800

    Add the logos for mobile and desktop viewports provide the globals.css also with a change of smooth loading and some basic stylings for buttons grabbed from airbnb

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -13,7 +13,12 @@ const NavBar = () => {
     return (
         <nav className='fixed w-full z-10 flex items-center justify-between px-6 py-4 bg-white shadow-md'>
             <Link href='/'>
-                <Image src='/assets/logo.png' alt='Logo' width={150} height={150} />
+                <div className='hidden md:block'>
+                    <Image src='/assets/logo-desktop.png' alt='Logo' width={228} height={50} />
+                </div>
+                <div className='md:hidden'>
+                    <Image src='/assets/logo-mobile.png' alt='Logo' width={50} height={50} />
+                </div>
             </Link>
         </nav>
     );
