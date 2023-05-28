2c32969d84ab18fd11a1dc208def11272227ef86
Eric Sidibe <eric@sidibe.de>
2023-05-28 14:12:52 +0800

    Implement UserAvatarButton component adjust and some size adjustments for the navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,20 +8,22 @@
 //  - Search
 import Link from "next/link";
 import Image from "next/image";
+import UserAvatarButton from "./UserAvatarButton";
 
 const NavBar = () => {
-    return (
-        <nav className='fixed w-full z-10 flex items-center justify-between px-6 py-4 bg-white shadow-md'>
-            <Link href='/'>
-                <div className='hidden md:block'>
-                    <Image src='/assets/logo-desktop.png' alt='Logo' width={228} height={50} />
-                </div>
-                <div className='md:hidden'>
-                    <Image src='/assets/logo-mobile.png' alt='Logo' width={50} height={50} />
-                </div>
-            </Link>
-        </nav>
-    );
-}
+  return (
+    <nav className="fixed w-full z-10 flex items-center justify-between px-6 py-3 bg-white shadow-md">
+      <Link href="/">
+        <div className="hidden md:block">
+          <Image src="/assets/logo-desktop.png" alt="Logo" width={200} height={44} />
+        </div>
+        <div className="md:hidden">
+          <Image src="/assets/logo-mobile.png" alt="Logo" width={40} height={40} />
+        </div>
+      </Link>
+      <UserAvatarButton />
+    </nav>
+  );
+};
 
 export default NavBar;
---
+++ components/UserAvatarButton.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+const UserAvatarButton = () => {
+  return (
+    <button className="flex items-center justify-between p-1.5 rounded-full border border-gray-300 hover:bg-gray-100 w-24">
+      <div className="ml-2">
+        <Image
+          src="/assets/menu-icon.svg"
+          alt="Menu Icon"
+          width={20}
+          height={20}
+          className="text-gray-500"
+        />
+      </div>
+      <div className="relative flex-shrink-0 w-8 h-8 rounded-full overflow-hidden">
+        <Image
+          src="/assets/user-avatar.png"
+          alt="User Avatar"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </button>
+  );
+};
+
+export default UserAvatarButton;
