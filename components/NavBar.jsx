2562991a160a3372ca94a24da602d09bea2dfacc
Eric Sidibe <eric@sidibe.de>
2023-05-28 14:18:18 +0800

    make a small responsive adjustment for the navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -12,18 +12,22 @@ import UserAvatarButton from "./UserAvatarButton";
 
 const NavBar = () => {
   return (
-    <nav className="fixed w-full z-10 flex items-center justify-between px-6 py-3 bg-white shadow-md">
-      <Link href="/">
-        <div className="hidden md:block">
-          <Image src="/assets/logo-desktop.png" alt="Logo" width={200} height={44} />
+        <nav className="fixed w-full z-10 px-6 sm:px-12 py-3 bg-white shadow-md">
+        <div className="max-w-screen-xl mx-auto flex items-center justify-between">
+            <Link href="/">
+            <div>
+                <div className="hidden md:block">
+                <Image src="/assets/logo-desktop.png" alt="Logo" width={200} height={44} />
+                </div>
+                <div className="md:hidden">
+                <Image src="/assets/logo-mobile.png" alt="Logo" width={40} height={40} />
+                </div>
+            </div>
+            </Link>
+            <UserAvatarButton />
         </div>
-        <div className="md:hidden">
-          <Image src="/assets/logo-mobile.png" alt="Logo" width={40} height={40} />
-        </div>
-      </Link>
-      <UserAvatarButton />
-    </nav>
-  );
+        </nav>
+    );
 };
 
 export default NavBar;
