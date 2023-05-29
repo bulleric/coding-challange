1846d9b84b2869396a5385dcd8beaee4e93d04c3
Eric Sidibe <eric@sidibe.de>
2023-05-29 11:05:56 +0800

    Document some of the requirements for the listing page

---
+++ app/listing/page.js
@@ -0,0 +1,61 @@
+// Listing page requriements
+// - Header and title with relevant content (placeholders are fine)
+// - Images and easy way to view them all
+// - Home title with Host image
+// - Description section
+// - Amenities Section
+// - Location Section
+// - Reservation widget sticky on the right side beneath images (does not have to be functional, but should show check in and check out dates).
+// - Well designed and responsive on mobile
+{/* 
+    Mobile View
+        ** Navbar back button
+        ** Image on top
+        ** Title
+        ** Row with Star, rating, x reviews, superhost, location
+        ** Row with Star, rating, x reviews, superhost, location
+        ** Reserver footer
+        * Info row
+        * headding 2 rooms
+        * icon rows
+        * about the guestkeeper
+        * about the location
+        * where you can sleep 
+        * the offerings of the location
+        * map 
+        * location description
+        * calender
+        * reviews
+        * additional info section
+    Desktop view
+        ** Title
+        ** Row with Star, rating, x reviews, superhost, location, share and save
+        ** Image gallery    
+            * 1 image on the left
+            * 4 images on the right ( grid )
+        ** Headline room in a home hostet by <hostname> (AvatarImage)
+        ** Icon row horizontal ( king bed, Dedicated bathroom... )
+        ** icon row vertical ( airbnb plus, room in a home...)
+        ** right side reserve box 
+        * about the location
+        * where you can sleep 
+        * the offerings of the location
+        * map 
+        * location description
+        * calender
+        * reviews
+        * additional info section
+*/}
+
+import React from 'react'
+
+const Listing = () => {
+  return (
+    <section className="mt-24 sm:mt-20 px-6 sm:px-12">
+        <h1>Privatzimmer Nr.2, gute Lage, ruhige Gegend, Canggu </h1>
+    </section>
+
+  )
+}
+
+export default Listing
