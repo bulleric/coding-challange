e160c7a5dc591f4ce64278c0c923a549dc57723d
Eric Sidibe <eric@sidibe.de>
2023-05-30 09:42:17 +0800

    Remove outer classes with bg-white and pt-1 from ImageGrid also add a rounded corner adjustment that the image is displayed correctly for mobile

--- components/ImageGrid.jsx
+++ components/ImageGrid.jsx
@@ -19,49 +19,45 @@ const images = [
 ]
 const ImageGrid = () => {
   return (
-    <div className="bg-white">
-      <div className="pt-1">
-        <div className="mx-auto mt-6 sm:px-6 lg:grid lg:grid-cols-2 lg:gap-3 lg:px-2">
-          <div className="aspect-h-4 aspect-w-3 overflow-hidden rounded-tl-lg rounded-bl-lg">
+      <div className="mx-auto mt-6 sm:px-6 lg:grid lg:grid-cols-2 lg:gap-3 lg:px-0">
+        <div className="aspect-h-4 aspect-w-3 overflow-hidden rounded-lg lg:rounded-none lg:rounded-tl-lg lg:rounded-bl-lg">
+          <img
+            src={images[0].src}
+            alt={images[0].alt}
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
+        <div className="hidden lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:gap-3">
+          <div className="aspect-h-2 aspect-w-3 overflow-hidden">
             <img
-              src={images[0].src}
-              alt={images[0].alt}
+              src={images[1].src}
+              alt={images[1].alt}
               className="h-full w-full object-cover object-center"
             />
           </div>
-          <div className="hidden lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:gap-3">
-            <div className="aspect-h-2 aspect-w-3 overflow-hidden">
-              <img
-                src={images[1].src}
-                alt={images[1].alt}
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-            <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-tr-lg">
-              <img
-                src={images[1].src}
-                alt={images[1].alt}
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-            <div className="aspect-h-2 aspect-w-3 overflow-hidden ">
-              <img
-                src={images[1].src}
-                alt={images[1].alt}
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-            <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-br-lg">
-              <img
-                src={images[1].src}
-                alt={images[1].alt}
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
+          <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-tr-lg">
+            <img
+              src={images[1].src}
+              alt={images[1].alt}
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+          <div className="aspect-h-2 aspect-w-3 overflow-hidden ">
+            <img
+              src={images[1].src}
+              alt={images[1].alt}
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+          <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-br-lg">
+            <img
+              src={images[1].src}
+              alt={images[1].alt}
+              className="h-full w-full object-cover object-center"
+            />
           </div>
         </div>
       </div>
-    </div>
   )
 }
 
